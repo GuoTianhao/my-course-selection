ec8dcc6e82cc3677342c4cf39adfa95e2262278f
hanxvdovehx@gmail.com <hanxvdovehx@gmail.com@66afb390-fe40-a6f5-b012-9e7fd0482df1>
2013-11-24 08:42:33 +0000

    Student界面跳转

--- trunk/ my-course-selection/CourseSelectionClient/src/com/client/ui/studentUI/StudentUISwitchController.java
+++ trunk/ my-course-selection/CourseSelectionClient/src/com/client/ui/studentUI/StudentUISwitchController.java
@@ -4,6 +4,10 @@ import java.awt.Point;
 
 import com.client.ui.loginUI.Login;
 import com.client.ui.main.MainFrame;
+import com.client.ui.studentUI.CScore.ScoreCheckPanel;
+import com.client.ui.studentUI.MCourse.MyCoursePanel;
+import com.client.ui.studentUI.SCourse.CourseSelectedPanel;
+import com.client.ui.studentUI.lCourse.CourseLookUpPanel;
 
 public class StudentUISwitchController {
 	private static StudentUISwitchController controller = null;
@@ -27,6 +31,30 @@ public class StudentUISwitchController {
 		frame.refresh();
 	}
 	
+	public void switchToScoreCheck(){
+		frame.getContentPane().removeAll();
+		frame.add(new ScoreCheckPanel(new Point(0,0),frame.getSize()));
+		frame.refresh();
+	}
+	
+	public void switchToAllCourse(){
+		frame.getContentPane().removeAll();
+		frame.add(new CourseLookUpPanel(new Point(0,0),frame.getSize()));
+		frame.refresh();
+
+	}
+	
+	public void switchToMCourse(){
+		frame.getContentPane().removeAll();
+		frame.add(new MyCoursePanel(new Point(0,0),frame.getSize()));
+		frame.refresh();
+	}
+	
+	public void switchToSCourse(){
+		frame.getContentPane().removeAll();
+		frame.add(new CourseSelectedPanel(new Point(0,0),frame.getSize()));
+		frame.refresh();
+	}
 	public void dispose(){
 		if (frame != null) {
 			frame.dispose();
